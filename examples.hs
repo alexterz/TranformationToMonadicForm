1c38c134baa612a3502ba0c77e65410086e563be
Alexandra <alex.terzidaki@gmail.com>
2019-04-24 13:04:14 +0300

    Edit Desugaring

---
+++ examples.hs
@@ -0,0 +1,20 @@
+
+
+--isws provlima an t teleutaio argument einai literal, qvar, gcon 
+
+
+add':: (Monad m, Num t1, Num t2) => (t1 -> m (t2 -> m (m b))) -> m b
+
+add' add = add 5 >>= \g2 -> g2 6 >>= \g1 -> g1 >>= \h -> return h
+
+
+
+
+add'' :: (Monad m, Num t1, Num t2) => (t1 -> m (t2 -> m b)) -> m b
+add'' add = add 5 >>= \g2 -> g2 6 >>= \h -> return h
+
+
+-- προβλημα αν expr = literal
+
+t:: Monad m => m Int
+--t = 5 >>=  \h -> return h 
