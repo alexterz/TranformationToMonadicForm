bff2396f51886e48f00401dd2f927efae03f7501
Alexandra <alex.terzidaki@gmail.com>
2019-06-06 16:16:48 +0300

    Recursion for one function definition, using fixpoint

--- transformation/src/Eval.hs
+++ transformation/src/Eval.hs
@@ -6,54 +6,78 @@ module Eval (
 import Syntax
 
 import Control.Monad.Except
+import Data.Function
 import qualified Data.Map as Map
 
 data Value
   = VInt Int
   | VBool Bool
   | VClosure [Apats] Expr (Eval.Scope)
--- | Values [Value]
+
 
 
 
 instance Show Value where
   show (VInt x) = show x
   show (VBool x) = show x
-  show VClosure{} = "<<closure>>"
---  show (Values {}) = "<<>values>"
+  show (VClosure l expr env) =  "<<closure>>: /"++show l ++"->" ++ show expr
 
 
 
 type Eval t = Except String t
 
+
 type Scope = Map.Map String [Value] -- me case [args] of tha kanw to pattern matching
+
 
 --type Definition = Value
 
 runMain :: [Dclr] -> Either String [Value]
-runMain x = runExcept $ evalMain emptyScope x
+runMain x =let
+              env' = updateCombY emptyScope--extendDef emptyScope "combY" [VClosure [Var "f"] (App (Apat (Var "f")) (Lam [Var "x"] (App (App (Apat (Var "combY")) (Apat (Var "f"))) (Apat (Var "x"))))) emptyScope]
+           in 
+              runExcept $ evalMain env' x
+
+updateCombY:: Scope -> Scope
+updateCombY env = Map.insert "combY" [VClosure [Var "f"] (App (Apat (Var "f")) (Lam [Var "x"] (App (App (Apat (Var "combY")) (Apat (Var "f"))) (Apat (Var "x"))))) env] env
 
 evalMain :: Eval.Scope -> [Dclr] -> Eval [Value]
 --given some declarations, evaluates the last one (e.g main)
-evalMain env [(Assign name args expr)] = eval env expr -- edw mallon tha kanw to pattern matching, pros to paron asto, me noiazei mono o orismos
+evalMain env [(Assign name args expr)] = eval env expr 
 evalMain env (x:xs) = do 
                       env' <- assign [x] env
                       evalMain env' xs 
+{--
+fixapply _ [] =  []
+fixapply x (l:ls) = case l of
+                      (VClosure args body env)-> let l'= (apply [VClosure args body env] x) in  l':(fixapply x ls)
+                      otherwise ->  l:(fixapply x ls)
+--}
 
 eval :: Eval.Scope -> Expr -> Eval [Value]
 eval env expr = case expr of
-  Apat (Lit (LInt x)) -> return $ [VInt (fromIntegral x)]
+  Apat (Lit (LInt x)) -> return $ [VInt x]
   Apat (Lit (LBool x)) -> return $ [VBool x]
-  Apat (Var x) -> case (Map.lookup x env) of
-                          Just (l:ls) ->  return $ l:ls
-                          Nothing -> throwError ("Can't find Variable"++ show x)
-  Lam x body -> return [VClosure x body env]
+  Apat (Var x) -> case x of
+                     ("combY")-> return [VClosure [Var "f"] (App (Apat (Var "f")) (Lam [Var "x"] (App (App (Apat (Var "combY")) (Apat (Var "f"))) (Apat (Var "x"))))) env] 
+                     otherwise ->  case (Map.lookup x env) of
+                                        Just (l:ls) -> return $ (l:ls) -- fixapply x (l:ls)
+                                        Nothing ->  throwError ("Can't find Variable "++ show x ++ " in the env " ++ show env)
+  Lam x body -> return [VClosure x body env]----apply (fixClosure env) [VClosure x body env]--tryfix--return [VClosure x body env]----
   App a b -> do
-    y <- eval env b   
-    x <- eval env a--tryMatch a y env
-    apply x y
-    --x <- eval env a
-    --check a b x y env         
+    y  <- eval env b  
+    x  <- eval env a
+  --  comb <- eval env (Apat (Var "combY"))
+    x' <- (applyCombY x)
+ --   case x' of 
+ --     [VClosure args expr env']-> throwError ("Applying "++ show x' ++ "to "++ show y++ " with env " ++ show env'++"\n") 
+    apply x' y
+
+{--
+    case y of
+      [VInt l]   -> if (l==0) then throwError ("Ok") else apply x y
+      otherwise -> apply x y
+--}         
   Op op a b -> do
     x <- eval env a
     y <- eval env b
@@ -62,6 +86,39 @@ eval env expr = case expr of
                     env' <- assign dclrs env
                     eval env' expr    
 
+eval1 :: Eval.Scope -> Expr -> Eval [Value]
+eval1 env expr = case expr of
+  Apat (Lit (LInt x)) -> return $ [VInt x]
+  Apat (Lit (LBool x)) -> return $ [VBool x]
+  Apat (Var x) -> case x of
+                     ("combY")-> return [VClosure [Var "f"] (App (Apat (Var "f")) (Lam [Var "x"] (App (App (Apat (Var "combY")) (Apat (Var "f"))) (Apat (Var "x"))))) env] 
+                     otherwise ->  case (Map.lookup x env) of
+                                        Just (l:ls) -> return $ (l:ls) -- fixapply x (l:ls)
+                                        Nothing ->  throwError ("Can't find Variable "++ show x ++ " in the env " ++ show env)
+  Lam x body -> return [VClosure x body env]----apply (fixClosure env) [VClosure x body env]--tryfix--return [VClosure x body env]----
+  App a b -> do
+    y  <- eval1 env b  
+    x  <- eval1 env a
+  --  comb <- eval env (Apat (Var "combY"))
+ --   x' <- (applyCombY x)
+ --   case x' of 
+ --     [VClosure args expr env']-> throwError ("Applying "++ show x' ++ "to "++ show y++ " with env " ++ show env'++"\n") 
+    apply x y
+
+{--
+    case y of
+      [VInt l]   -> if (l==0) then throwError ("Ok") else apply x y
+      otherwise -> apply x y
+--}         
+  Op op a b -> do
+    x <- eval1 env a
+    y <- eval1 env b
+    binop op x y
+  Let dclrs expr -> do 
+                    env' <- assign dclrs env
+                    eval1 env' expr 
+
+
 
 -- takes a list of declarations and an env and returns an updated env'
 assign:: [Dclr] -> Scope ->Eval Scope 
@@ -69,7 +126,7 @@ assign [] env = return env
 assign ((Assign name args expr'):xs) env = 
                      let  value =  runExcept $ eval env expr'
                      in   case value of 
-                              Left err -> throwError "Error on the assigned expression in declaration"
+                              Left err -> throwError (err ++ " Error on the assigned expression in declaration")              
                               Right val -> assign xs env'
                                        where 
                                        env' = extendDef env name val  --mhpws gia anadromh thelei kai sto right hand env'
@@ -88,6 +145,7 @@ extendDef env v t = case (Map.lookup v env) of
                       Just l-> Map.insert v list env -- list contains all the definitions of the form ([args],Value) for a function with name v
                               where list = l ++ t
                       Nothing ->  Map.insert v t env
+ 
 
 
 -- updates the local env
@@ -95,22 +153,36 @@ extendLocal :: Scope -> String -> [Value] -> Scope
 extendLocal env v t = Map.insert v t env
 
 
+applyCombY:: [Value]-> Eval [Value]
+applyCombY [VClosure args expr env] = --eval (extendLocal env ("f") [VClosure args expr env]) t1
+                                      --  where t1 = (App (Apat (Var "f")) (Lam [Var "x"] (App (App (Apat (Var "combY")) (Apat (Var "f"))) (Apat (Var "x")))))                 
+                                      let env' = (extendLocal env ("f") [VClosure args expr env])
+                                          env''= (extendLocal env' ("f") [VClosure args expr env']) 
+                                      in
+                                         do
+                                            --throwError ("in ApplyCombY env :" ++ show env'')
+                                            x<- eval env'' (Apat(Var "f")) 
+                                            y<- eval env'' (Lam [Var "x"] (App (App (Apat (Var "combY")) (Apat (Var "f"))) (Apat (Var "x"))))
+                                            apply x y  
+
 --apply the function to the new env
---edw prepei na metatrepsw to Apats se String
+--edw prepei na kanw elegxo gia typo kai plhthos orismatwn giati mexri stigmhs mporei na oristei h f x =x ; f 1 0 = 10
 apply :: [Value] -> [Value] -> Eval [Value]
+--for one argument (v)
 apply [VClosure [v] t0 e] [t1] = case v of
-                                   Var name -> eval (extendLocal e name [t1]) t0
+                                   Var name -> eval1 (extendLocal e name [t1]) t0 --do throwError ("eval closure:"++show [t0]++ "\n"++ "old env:" ++ show e ++ "\n new env: "++ show(extendLocal e name [t1]) )-- 
                                    Lit (LInt literal) -> case t1 of 
                                                            (VInt x) -> if (x==literal) 
-                                                                       then eval e t0 
+                                                                       then eval1 e t0 
                                                                        else return []--throwError"Error on pattern matching 1" 
                                                            otherwise -> throwError("Cant match "++ show t1 ++ " with Int")
-                             --  otherwise -> eval e t0  
+                             --  otherwise -> eval e t0
+--for more than one args (v:vs)  
 apply [VClosure (v:vs) t0 e] [t1] =  case v of
-                                       Var name -> eval (extendLocal e name [t1]) (Lam vs t0) 
+                                       Var name -> eval1 (extendLocal e name [t1]) (Lam vs t0) 
                                        Lit (LInt literal) -> case t1 of 
                                                               (VInt x) -> if (x==literal) 
-                                                                          then eval e (Lam vs t0) 
+                                                                          then eval1 e (Lam vs t0) 
                                                                           else return [] --throwError"Error on pattern matching" 
                                                               otherwise -> throwError ("Cant match "++ show t1 ++ " with Int")
                              --   otherwise -> eval e (Lam vs t0) 
@@ -128,6 +200,38 @@ emptyScope = Map.empty
 
 runEval :: Expr -> Either String [Value]
 runEval x = runExcept (eval emptyScope x)
+
+
+{--
+newtype Mu a = Roll (Mu a -> (a -> a))
+unroll (Roll x) = x
+
+fix :: (a -> a) -> a -> a
+fix = \f ->       (\x z -> f ((unroll x) x z))
+            (Roll (\x z -> f ((unroll x) x z)))
+
+fixClosure:: Scope -> [Value]
+fixClosure env= [VClosure [Var "f"] (App (Lam [Var "x",Var "z"] (App (Apat (Var "f")) (App (App (App (Apat (Var "unroll")) (Apat (Var "x"))) (Apat (Var "x"))) (Apat (Var "z"))))) (App (Apat (Var "Roll")) (Lam [Var "x",Var "z"] (App (Apat (Var "f")) (App (App (App (Apat (Var "unroll")) (Apat (Var "x"))) (Apat (Var "x"))) (Apat (Var "z"))))))) env]
+--(Apat (Lit (LInt 0))) emptyScope
+--}
+data Paradox a = Self (Paradox a -> a)
+fixpoint'' = \f -> let half (Self twin) = f (twin (Self twin))
+                   in half (Self half)
+
+
+{--
+tryfix:: Expr
+tryfix = let 
+             f = \ff x -> return $ [VInt 1] --x =return $[VInt 1]
+         in
+             fix f   --}
+
+fixClosure:: Scope -> [Value]
+fixClosure env= [VClosure [Var "f"] (App (Lam [Var "x"] (App (Apat (Var "f")) (App (Apat (Var "x")) (Apat (Var "x"))))) (Lam [Var "x"] (App (Apat (Var "f")) (App (Apat (Var "x")) (Apat (Var "x"))))))  env]
+
+--fixpoint = \f -> ((\x -> f (x x)) (\x -> f (x x)))
+
+--fixpoint f x = f (fixpoint f) x
 
 {--
 --an to y den einai Value alla string apo error???
